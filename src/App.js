0a5f6e91c055dd86d3da11eaafda12b9e1a9fe1d
toprednax <toprednax@yahoo.com>
2022-02-15 16:37:18 +0100

    poging 1 op knop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="bg-blue-700 text-3xl fond-bold underline">
+        Hallo wereld
+      </button>
+    </div>
+  );
+};
+export default Button;
