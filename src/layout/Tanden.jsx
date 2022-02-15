63a4a78c5a4edbf6d3fbf2e6c73a50586dae7747
JDogM <matt.mll@hotmail.com>
2022-02-15 16:45:30 +0100

    Tanden toegevoegd

---
+++ src/layout/Tanden.jsx
@@ -0,0 +1,23 @@
+import './tanden.css';
+
+function Tanden() {
+	return (
+		<div className="Tanden">
+			<header className="Tanden-header">
+				<p>
+					Edit <code>src/tanden.jsx</code> and save to reload.
+				</p>
+				<a
+					className="Tanden-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default Tanden;
