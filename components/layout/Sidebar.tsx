4dc473431c34d4d2be5b9269f60ce4551ba2260d
JDogM <matt.mll@hotmail.com>
2022-03-01 17:38:47 +0100

    Aanpassing layout

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -14,21 +14,23 @@ const Sidebar = () =>
 	return(
 		<div className="min-w-screen min-h-screen flex justify-end">
 			{!swiped ?
-				<div className="w-1/5 divide-y-2 divide-gray-400">
+				<div className="w-1/5 divide-y-2 divide-gray-400 overflow-hidden">
 					<div className="bg-gray-400">
-						<button onClick={onSwipe}> <Back value={swiped}/> </button>
+						<button onClick={onSwipe}> 
+							<Back value={swiped}/> 
+						</button>
 					</div>
 
 					<div className="h-1/2 bg-gray-100"> 
 						<Anatomy />
 					</div>
 
-					<div className="bg-gray-100 min-h-screen">
+					<div className="bg-gray-100 h-1/2">
 						<Display />
 					</div>
 				</div>
 				:
-				<div className="w-1/5 divide-y-2 divide-gray-400">
+				<div className="w-1/10 divide-y-2 divide-gray-400">
 					<div className="bg-gray-400">
 						<button onClick={onSwipe}> 
 							<Back value={swiped}/>
--- components/layout/Tanden_1x.jsx
+++ components/layout/Tanden_1x.jsx
@@ -1,3 +1,5 @@
+// import SVG_STRING_PER_STRUCTURE from "../textures/AnatomySvgData.js"
+
 const Tanden = () => 
 {
 	return (
@@ -5,6 +7,7 @@ const Tanden = () =>
 			<button>
 				<div className="flex flex-col">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						{/* <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={SVG_STRING_PER_STRUCTURE[MAXILLA].path} /> */}
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" />
 					</svg>
 					<p>18</p>
