7387f828e9c1d2d3ac478253d818734e33fe8ece
Ruben Hias <ruben.hias@gmail.com>
2022-03-01 14:14:14 +0100

    Start of file overview

---
+++ components/filecard.tsx
@@ -0,0 +1,21 @@
+import { IFile } from "../types";
+import { AiOutlineFile } from "react-icons/ai";
+import Link from 'next/link';
+
+type FileCardProps = {
+    file: IFile;
+};
+
+export default function FileCard({ file }: FileCardProps) {
+    return (
+        <Link href="/">
+            <div className="flex items-center">
+                <AiOutlineFile className="text-5xl" />
+                <div className="w-80 text-gray-700 p-5">
+                    <div className="text-2xl mb-2">{file.title}</div>
+                    <div></div>
+                </div>
+            </div>
+        </Link>
+    );
+}
