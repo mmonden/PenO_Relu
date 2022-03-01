25d514de62cab4d5f93e264cf4c4cc9f13d0905f
RobWi01 <rob.windey@gmail.com>
2022-03-01 09:43:12 +0100

    Addition of sharebutton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import type { NextPage } from 'next'
 import AnnotationBar from '../components/annotationbar'
 import { getAnnotations, writeAnnotation } from '../lib/annotations'
 import { GetServerSideProps } from 'next';
+import PopUp from '../components/PopUp';
+;
 
 export default function Home({annotations}) {
 
   return (
     <div className="min-h-screen min-w-screen">
       <AnnotationBar cardsInput={annotations}/>
+      <div className="flex justify-end items-end absolute inset-0 z-10">
+      <PopUp />
+      </div>
     </div>
+  )
+}
+
+export const Home1: NextPage = () => {
+  return (
+    <PopUp />
   )
 }
 
