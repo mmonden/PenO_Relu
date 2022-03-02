4ada426cd00fde1902085f53478f4deda5734b7b
JDogM <matt.mll@hotmail.com>
2022-03-02 14:57:44 +0100

    merge conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import AnnotationBar from '../components/annotationbar'
 import { getAnnotations, writeAnnotation, getFiles} from '../lib/annotations'
 import { GetServerSideProps } from 'next';
-<<<<<<< HEAD
 import PopUp from '../components/PopUp';
 import Sidebar from '../components/layout/Sidebar';
 
-=======
 import Stlviewer from '../components/stlviewer';
-import PopUp from '../components/PopUp'
->>>>>>> 35f66b05fe27823e22347ed989880833792689ef
 
 export default function Home({annotations, files}) {
 
@@ -16,14 +12,7 @@ export default function Home({annotations, files}) {
     <div className="min-h-screen min-w-screen">
       <AnnotationBar cardsInput={annotations}/>
       <div className="flex justify-end items-end absolute inset-0 z-10">
-<<<<<<< HEAD
         <PopUp />
-=======
-      <PopUp file={files[0]} />
-      </div>
-      <div>
-      <Stlviewer />
->>>>>>> 35f66b05fe27823e22347ed989880833792689ef
       </div>
       <Sidebar />
     </div>
