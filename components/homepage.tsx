1764c48b17a68f32f971583694f75da433c52656
thomaskeuppens <thomaskeuppens@gmail.com>
2022-03-22 17:34:25 +0100

    extra push

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -15,14 +15,6 @@ type HomePageProps = {
 export default function HomePage({ patients_input, files_input }: HomePageProps) {
     const patient1Logged = files_input.filter((file) => patients_input[0].file_ids.some((id) => file._id == id))
     const [loggedFiles, setLoggedFiles] = useState(patient1Logged);
-    console.log(patient1Logged);
-    //setLoggedFiles(files_input.filter((file) => patients_input[0].file_ids.includes(file._id)));
-    // console.log(files_input)
-    // console.log(patients_input[0]);
-    // console.log(patients_input[0].file_ids.includes("1"))
-    // console.log("file_ids: ", patients_input[0].file_ids)
-    // console.log("logged files", loggedFiles)
-    // console.log("patient1: ", patient1_files)
     return (
         <div className="min-w-screen min-h-screen flex flex-col items-start m-8">
             <div className="absolute top-12">
