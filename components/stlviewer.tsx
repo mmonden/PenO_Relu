3ec832cfbaf4f1e1bfca8e601d76c25e73d34507
Thomas-de-Ruijter <78972348+Thomas-de-Ruijter@users.noreply.github.com>
2022-04-04 12:53:36 +0200

    cleaned up + remove line + added some more stls

--- components/stlviewer.tsx
+++ components/stlviewer.tsx
@@ -16,15 +16,13 @@ type FileCardProps = {
   file: IFile;
 };
 
-
-let camera, scene, controls, renderer, followLight, light
+let camera, scene, controls, renderer, followLight, light, theline;
 
 export default function Stlviewer({ file }: FileCardProps) {
   const threeContainerRef = useRef(null);
 
   useEffect(() => {
-
-    init()
+    init();
 
     //add Container to renderer
     threeContainerRef.current.appendChild(renderer.domElement);
@@ -63,24 +61,25 @@ export default function Stlviewer({ file }: FileCardProps) {
           opacity: 1.0,
           transparent: true,
         });
-        materials.push(materialTooth)
+        materials.push(materialTooth);
       }
     }
 
     for (var x = 1; x < 5; x++) {
       for (var y = 1; y < 7; y++) {
-        let filename = "Tooth_".concat(x.toString()).concat(y.toString())
+        let filename = "Tooth_".concat(x.toString()).concat(y.toString());
 
         loader.load(
           "http://localhost:3000/" + filename + ".stl",
           function (geometry) {
+            let toothNr = parseInt(filename.split("_").pop());
+            let a = Math.floor(toothNr / 10);
+            let b = toothNr % 10;
 
-            let toothNr = parseInt(filename.split('_').pop())
-            let a = Math.floor(toothNr / 10)
-            let b = toothNr % 10
-            console.log(a, b)
-
-            const mesh = new THREE.Mesh(geometry, materials[(a - 1) * 6 + (b - 1)]);
+            const mesh = new THREE.Mesh(
+              geometry,
+              materials[(a - 1) * 6 + (b - 1)]
+            );
             scene.add(mesh);
           },
           (xhr) => {
@@ -119,28 +118,36 @@ export default function Stlviewer({ file }: FileCardProps) {
         scene.children = scene.children.filter(
           (child) => !(child instanceof Sprite)
         );
+        if (theline) {
+          scene.remove(theline)
+        }
 
-        //variabeles for determining the postion of the text label and corresponding line 
+        //variabeles for determining the postion of the text label and corresponding line
         const startingpoint = [25, -10, -40]; //eventually to be done by clicking the screen
         const endpoint = [60, 0, 10]; //idem
         //start of code for lines
 
-        const material = new THREE.LineBasicMaterial( { 
+        const material = new THREE.LineBasicMaterial({
           color: new THREE.Color(0x000000),
-          linewidth: 1, });
+          linewidth: 1,
+        });
 
         const points = [];
-      
 
-        points.push( new THREE.Vector3( startingpoint[0], startingpoint[1], startingpoint[3] ) ); 
-        points.push( new THREE.Vector3( endpoint[0] , endpoint[1] , endpoint[2] ) );
+        points.push(
+          new THREE.Vector3(
+            startingpoint[0],
+            startingpoint[1],
+            startingpoint[3]
+          )
+        );
+        points.push(new THREE.Vector3(endpoint[0], endpoint[1], endpoint[2]));
 
-        const geometry = new THREE.BufferGeometry().setFromPoints( points );
-    
-        const line = new THREE.Line( geometry, material );
-        scene.add( line );
-      //scene.remove(line);  deze commande verwijderd de lijn
-      //start of code for lines
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+        theline = new THREE.Line(geometry, material);
+        scene.add(theline);
+        //end of code for lines
 
         //start code for textlabel
         var tekstlabel = makeTextSprite(title, {
@@ -151,9 +158,6 @@ export default function Stlviewer({ file }: FileCardProps) {
         tekstlabel.position.set(endpoint[0] + 5, endpoint[1], endpoint[2]); //Define sprite's anchor point
         scene.add(tekstlabel);
         //end code for text label
-
-          
-
       }
     });
 
@@ -189,14 +193,15 @@ export default function Stlviewer({ file }: FileCardProps) {
 
           for (var i = 0; i < scene.children.length; i++) {
             if (scene.children[i] instanceof THREE.Mesh) {
-              // console.log("Child", i, scene.children[i])
-              // console.log("Intersect", j, intersects[j].object)
-              if (typeof intersects[0] !== 'undefined' && scene.children[i] == intersects[0].object) {
+              if (
+                typeof intersects[0] !== "undefined" &&
+                scene.children[i] == intersects[0].object
+              ) {
                 // @ts-ignore
-                (scene.children[i] as THREE.Mesh).material.color.set(0x00ff00)
+                (scene.children[i] as THREE.Mesh).material.color.set(0x00ff00);
               } else {
                 // @ts-ignore
-                (scene.children[i] as THREE.Mesh).material.color.set(0xd3d3d3)
+                (scene.children[i] as THREE.Mesh).material.color.set(0xd3d3d3);
               }
             }
           }
@@ -219,36 +224,13 @@ export default function Stlviewer({ file }: FileCardProps) {
       }
     );
 
-    console.log(scene.children)
-
-    /// Begin click vs drag v2
-    // const delta = 2;
-    // let startX;
-    // let startY;
-
-    // document.addEventListener('mousedown', function (event) {
-    //   startX = event.pageX;
-    //   startY = event.pageY;
-    // });
-
-    // document.addEventListener('mouseup', function (event) {
-    //   const diffX = Math.abs(event.pageX - startX);
-    //   const diffY = Math.abs(event.pageY - startY);
-
-    //   if (diffX < delta && diffY < delta) {
-    //     console.log("click!")
-    //   } else {
-    //     console.log("drag")
-    //   }
-    // });
-    /// End end vs drag v2
     animate();
   });
 
   return <div ref={threeContainerRef} />;
 }
 
-function init(){
+function init() {
   //creating scene
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
@@ -273,7 +255,7 @@ function init(){
   camera.position.set(0, -3, 3); // Set position like this
   //camera.rotation.set(0, 100, 0);
   camera.lookAt(new THREE.Vector3(5, 0, -57.33));
-  camera.rotateOnAxis(new THREE.Vector3(1, 0, 0),3.14)
+  camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), 3.14);
   camera.updateProjectionMatrix();
 
   //RENDERER
@@ -296,9 +278,9 @@ function render() {
   renderer.render(scene, camera);
 }
 
- //FUNCTION FOR BUTTONS IN "Tanden" TO ADAPT CAMARA PERSPECTIVE WHEN PUSHED ON
- function ChangePerspective(x,y,z) {
-  camera.position.set(x, y, z); 
+//FUNCTION FOR BUTTONS IN "Tanden" TO ADAPT CAMARA PERSPECTIVE WHEN PUSHED ON
+function ChangePerspective(x, y, z) {
+  camera.position.set(x, y, z);
   camera.updateProjectionMatrix();
-  render() //moet dit erbij?
-} 
+  render(); //moet dit erbij?
+}
