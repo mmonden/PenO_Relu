9f0a38ae5ea4c48d3b60a3024dc962c535d2c5da
thomaskeuppens <thomaskeuppens@gmail.com>
2022-04-20 22:36:17 +0200

    little changes

--- components/deleteModal.tsx
+++ components/deleteModal.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import { FcCheckmark, FcCancel } from "react-icons/fc";
 
+
+const OVERLAY_STYLES = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0, 0, .7)',
+    zIndex: 1000
+
+}
+
+const MODAL_STYLES = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, 50%)',
+    backgroundColor: '#FFF',
+    padding: '50px',
+    zIndex: 1000
+}
+
 export default function DeleteModal({ open, children, onClose, onDelete }) {
     if (!open) return null;
 
@@ -10,8 +32,8 @@ export default function DeleteModal({ open, children, onClose, onDelete }) {
                 {children}
             </div>
             <div>
-                <button onClick={onDelete}><FcCheckmark className="w-7 h-7" /></button>
                 <button onClick={onClose}><FcCancel className="w-7 h-7" /></button>
+                <button onClick={onDelete}><FcCheckmark className="w-7 h-7" /></button>
             </div>
         </div>
     )
