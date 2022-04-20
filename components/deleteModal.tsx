4cab0fa2d26adc13a3ea00581b1853d12dc51dc0
thomaskeuppens <thomaskeuppens@gmail.com>
2022-04-20 23:46:11 +0200

    popup in the middle

--- components/deleteModal.tsx
+++ components/deleteModal.tsx
@@ -5,7 +5,7 @@ import { FcCheckmark, FcCancel } from "react-icons/fc";
 const OVERLAY_STYLES = {
     position: 'fixed',
     top: 0,
-    left: 0,
+    left: -10,
     right: 0,
     bottom: 0,
     backgroundColor: 'rgba(0,0, 0, .7)',
@@ -15,25 +15,27 @@ const OVERLAY_STYLES = {
 
 const MODAL_STYLES = {
     position: 'fixed',
-    top: '50%',
-    left: '50%',
+    top: '30%',
+    left: '49%',
     transform: 'translate(-50%, 50%)',
     backgroundColor: '#FFF',
-    padding: '50px',
-    zIndex: 1000
+    padding: '40px',
+    zIndex: 1900
 }
 
 export default function DeleteModal({ open, children, onClose, onDelete }) {
     if (!open) return null;
 
     return (
-        <div>
-            <div>
-                {children}
-            </div>
-            <div>
-                <button onClick={onClose}><FcCancel className="w-7 h-7" /></button>
-                <button onClick={onDelete}><FcCheckmark className="w-7 h-7" /></button>
+        <div style={OVERLAY_STYLES}>
+            <div style={MODAL_STYLES}>
+                <div>
+                    {children}
+                </div>
+                <div>
+                    <button onClick={onClose}><FcCancel className="w-7 h-7" /></button>
+                    <button onClick={onDelete}><FcCheckmark className="w-7 h-7" /></button>
+                </div>
             </div>
         </div>
     )
