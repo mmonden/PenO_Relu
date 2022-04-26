50a24852176b9909565025fb3083487145033af6
CedricCamerman <cedric.camerman@student.kuleuven.be>
2022-04-26 14:38:46 +0200

    oude zoom functie

--- components/stlviewer.tsx
+++ components/stlviewer.tsx
@@ -300,7 +300,7 @@ function animate() {
 function render() {
   followLight.position.copy(camera.position);
 
-  camera.lookAt(new THREE.Vector3(5, 0, -57.33));
+  //camera.lookAt(new THREE.Vector3(5, 0, -57.33));
 
   renderer.render(scene, camera);
 }
